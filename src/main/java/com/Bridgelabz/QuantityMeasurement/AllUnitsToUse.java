8810735d79c1307cf8828bf7dddc90b142b25b70
spatil022 <shamalpatil1998@gmail.com>
2021-04-25 12:00:24 +0530

    [Shamal]DELETED: .idea file

--- src/main/java/com/Bridgelabz/QuantityMeasurement/AllUnitsToUse.java
+++ src/main/java/com/Bridgelabz/QuantityMeasurement/AllUnitsToUse.java
@@ -1,19 +1,2 @@
-package com.Bridgelabz.QuantityMeasurement;
-
-public class AllUnitsToUse {
-    public enum UnitConverter {
-        FEET {
-            public double convertToInches(double length) {
-                return length * 12;
-            }
-        },
-        INCHES {
-            public double convertToInches(double length) {
-                return length;
-            }
-        };
-        public double convertToInches(double length) {
-            return 0.0;
-        }
-    }
+package com.Bridgelabz.QuantityMeasurement;public class AllUnitsToUse {
 }
